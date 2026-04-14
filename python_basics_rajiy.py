caa4fbbf6f32453b0ce768455423a660f0d0d45c
rajiycareer <rajiy.career@gmail.com>
2026-04-14 01:13:15 -0500

    python-basics-assignment

---
+++ python_basics_rajiy.py
@@ -0,0 +1,16 @@
+name = "RY"
+age = 30
+height = 5.5
+is_student = True
+
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_squared = age ** 2
+
+print(f"Age in months: {age_in_months}")
+print(f"Age in days: {age_in_days}")
+print(f"Remainder when divided by 7: {remainder}")
+print(f"Age squared: {age_squared}")
